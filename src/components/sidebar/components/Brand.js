ab0a45a2fa4f45f4f36a27a32e748c0d3e82e582
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-11-23 22:09:53 +0000

    gjgjcjkjkjhvk

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -1,19 +1,27 @@
 import React from "react";
 
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, useColorModeValue, Image } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
+// Importing the image
+import logoSecu from 'components/icons/logosecu.png'; // Adjust the path if necessary
+
 export function SidebarBrand() {
-  //   Chakra color mode
+  // Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
 
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      <Image 
+        src={logoSecu}
+        h='26px' 
+        w='175px' 
+        my='32px'
+        alt='Logo' // Provide an alt description for accessibility
+      />
       <HSeparator mb='20px' />
     </Flex>
   );
