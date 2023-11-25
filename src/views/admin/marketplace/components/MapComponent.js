0aaff09d75a2de3640dbc510d09b9eea4c34657a
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-11-25 16:51:11 +0000

    hgdhfhfgh

--- src/views/admin/marketplace/components/MapComponent.js
+++ src/views/admin/marketplace/components/MapComponent.js
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const MapComponent = () => {
   const [location, setLocation] = useState({ lat: 0, lng: 0 });
+  const mapRef = useRef(null); // Ref to store the map instance
 
   useEffect(() => {
-    const map = L.map('map').setView([location.lat, location.lng], 13);
+    // Only initialize the map if it hasn't been created yet
+    if (!mapRef.current) {
+      mapRef.current = L.map('map').setView([location.lat, location.lng], 13);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(mapRef.current);
+    }
 
     // GPS Location Fetch
     navigator.geolocation.getCurrentPosition((position) => {
       const { latitude, longitude } = position.coords;
       setLocation({ lat: latitude, lng: longitude });
-      map.setView([latitude, longitude], 13);
+      if (mapRef.current) {
+        mapRef.current.setView([latitude, longitude], 13);
+      }
     });
-  }, []);
+  }, []); // Empty dependency array to run only once
 
   return <div id="map" style={{ height: '500px', width: '100%' }}></div>;
 };
