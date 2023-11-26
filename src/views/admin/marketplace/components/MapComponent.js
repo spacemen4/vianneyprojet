e6cab9517a938c93fd56c471fe13e96a7b1ff6e3
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-11-26 18:56:09 +0000

    uyjkyujkjhkhj

--- src/views/admin/marketplace/components/MapComponent.js
+++ src/views/admin/marketplace/components/MapComponent.js
@@ -1,36 +1,53 @@
 import React, { useState, useEffect, useRef } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client
+const supabaseUrl = 'https://hvjzemvfstwwhhahecwu.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2anplbXZmc3R3d2hoYWhlY3d1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTQ4Mjc3MCwiZXhwIjoyMDA3MDU4NzcwfQ.6jThCX2eaUjl2qt4WE3ykPbrh6skE8drYcmk-UCNDSw';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const MapComponent = () => {
-  const [location, setLocation] = useState({ lat: 0, lng: 0 });
   const mapRef = useRef(null); // Ref to store the map instance
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // Fetch users from the database
+    const fetchUsers = async () => {
+      let { data: usersOnGround, error } = await supabase
+        .from('vianney_users_on_the_ground')
+        .select('*');
+
+      if (error) {
+        console.error('Error fetching users:', error);
+      } else {
+        setUsers(usersOnGround);
+      }
+    };
+
+    fetchUsers();
+  }, []);
 
   useEffect(() => {
     if (!mapRef.current) {
-      mapRef.current = L.map('map').setView([0, 0], 13); // Set initial coordinates
+      mapRef.current = L.map('map').setView([0, 0], 13); // Initial map setup
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap contributors'
       }).addTo(mapRef.current);
     }
-  }, []); // Empty dependency array  
 
-  // Location Update
-  useEffect(() => {
-    // GPS Location Fetch
-    navigator.geolocation.getCurrentPosition((position) => {
-      const { latitude, longitude } = position.coords;
-      setLocation({ lat: latitude, lng: longitude });
-    });
-  }, []);
+    if (users.length > 0) {
+      // Center map on the first user
+      mapRef.current.setView([users[0].latitude, users[0].longitude], 13);
 
-  // Update map view when location changes
-  useEffect(() => {
-    if (mapRef.current) {
-      mapRef.current.setView([location.lat, location.lng], 13);
+      // Add markers for each user
+      users.forEach(user => {
+        L.marker([user.latitude, user.longitude]).addTo(mapRef.current);
+      });
     }
-  }, [location.lat, location.lng]);
+  }, [users]); // Dependency on users array
 
   return <div id="map" style={{ height: '500px', width: '100%' }}></div>;
 };
