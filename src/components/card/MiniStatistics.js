b5327472889ccb403ac4d301f8dc9c8e50ac7217
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-11-30 18:32:47 +0000

    hjnjhkhkjhhjjhhj

--- src/components/card/MiniStatistics.js
+++ src/components/card/MiniStatistics.js
@@ -1,11 +1,11 @@
-// Chakra imports
+import { FcAdvertising } from "react-icons/fc";
 // Chakra imports
 import {
   Flex,
   Stat,
   StatNumber,
   useColorModeValue,
-  Text,
+  // import other required components
 } from "@chakra-ui/react";
 // Custom components
 import Card from "components/card/Card.js";
@@ -13,7 +13,7 @@ import Card from "components/card/Card.js";
 import React from "react";
 
 export default function Default(props) {
-  const { startContent, endContent, growth, value } = props;
+  const {  event_name } = props;
   const textColor = useColorModeValue("secondaryGray.900", "white");
 
   return (
@@ -23,31 +23,20 @@ export default function Default(props) {
         h='100%'
         align={{ base: "center", xl: "start" }}
         justify={{ base: "center", xl: "center" }}>
-        {startContent}
+        {/* Replace existing icon with FcAdvertising icon */}
+        <FcAdvertising size="3em" />
 
-        <Stat my='auto' ms={startContent ? "18px" : "0px"}>
-          
+        <Stat my='auto' ms="10px" >
+
           <StatNumber
             color={textColor}
             fontSize={{
               base: "xl",
             }}>
-            {value}
+            {event_name}
           </StatNumber>
-          {growth ? (
-            <Flex align='center'>
-              <Text color='green.500' fontSize='xs' fontWeight='700' me='5px'>
-                {growth}
-              </Text>
-              <Text color='secondaryGray.600' fontSize='xs' fontWeight='400'>
-                since last month
-              </Text>
-            </Flex>
-          ) : null}
+          
         </Stat>
-        <Flex ms='auto' w='max-content'>
-          {endContent}
-        </Flex>
       </Flex>
     </Card>
   );
--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -1,48 +1,18 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 // Chakra imports
 import {
-  Avatar,
   Box,
-  Flex,
-  FormLabel,
   Icon,
-  Select,
   SimpleGrid,
   useColorModeValue,
 } from "@chakra-ui/react";
-// Assets
-import Usa from "assets/img/dashboards/usa.png";
 // Custom components
 import MiniCalendar from "components/calendar/MiniCalendar";
 import MiniStatistics from "components/card/MiniStatistics";
 import IconBox from "components/icons/IconBox";
 import React from "react";
 import {
-  MdAddTask,
   MdAttachMoney,
   MdBarChart,
-  MdFileCopy,
 } from "react-icons/md";
 import CheckTable from "views/admin/default/components/CheckTable";
 import ComplexTable from "views/admin/default/components/ComplexTable";
@@ -79,7 +49,7 @@ export default function UserReports() {
               }
             />
           }
-          value='Evenement 1'
+          event_name='Evenement 1'
         />
         <MiniStatistics
           startContent={
@@ -92,7 +62,7 @@ export default function UserReports() {
               }
             />
           }          
-          value='Evenement 2'
+          event_name='Evenement 2'
         />
       </SimpleGrid>
 
