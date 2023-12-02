bd9d7a2b4282e4539978eb8d7c2c37952d709abd
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-02 15:25:41 +0000

    ytyhthyjhyjhgjh

--- src/views/admin/default/DocumentionsComponent/components/PdfDownloadButton.js
+++ src/views/admin/default/DocumentionsComponent/components/PdfDownloadButton.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Box, Flex, Icon, Heading } from "@chakra-ui/react";
-import { FcDownload, FcDocument } from "react-icons/fc";
+import { Box, Flex, Icon, Heading, Stat, StatNumber, StatLabel, useColorModeValue } from "@chakra-ui/react";
+import { FcDownload, FcDocument, FcAdvertising } from "react-icons/fc";
 import { createClient } from "@supabase/supabase-js";
-import MiniStatistics from "../../../../../components/card/MiniStatistics"; // Import MiniStatistics
+import Card from "components/card/Card.js"; // Import Card
+import IconBox from "components/icons/IconBox"; // Import IconBox
 
 const supabaseUrl = "https://nhrsgicthwqsctwggxqz.supabase.co";
 const supabaseKey =
@@ -10,8 +11,11 @@ const supabaseKey =
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const PdfDownloadButton = ({ pdf_documents, handlePdfClick }) => { // Add pdf_documents as a prop
+const PdfDownloadButton = ({ handlePdfClick }) => {
   const [documents, setDocuments] = useState([]);
+  const brandColor = useColorModeValue("brand.500", "white");
+  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+  const textColor = useColorModeValue("secondaryGray.900", "white");
 
   useEffect(() => {
     const fetchDocuments = async () => {
@@ -26,14 +30,14 @@ const PdfDownloadButton = ({ pdf_documents, handlePdfClick }) => { // Add pdf_do
     fetchDocuments();
   }, []);
 
-  const handleDocumentClick = (data) => {
-    handlePdfClick(data);
+  // Function to format the date as desired
+  const formatDate = (dateString) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
   return (
     <Box>
-      
-
       <Box
         display="flex"
         flexDirection="column"
@@ -43,14 +47,28 @@ const PdfDownloadButton = ({ pdf_documents, handlePdfClick }) => { // Add pdf_do
         p="4"
       >
         {documents.map((data) => (
-          <MiniStatistics
-            key={data.id}
-            event_name={data.title}
-            date={data.description}
-            cursor="pointer"
-            onClick={() => handleDocumentClick(data)}
-          />
-          
+          <Card key={data.id} py='15px' cursor="pointer" onClick={() => handlePdfClick(data)}>
+            <Flex
+              my='auto'
+              h='100%'
+              align={{ base: "center", xl: "start" }}
+              justify={{ base: "center", xl: "center" }}>
+              <IconBox
+                w='56px'
+                h='56px'
+                bg={boxBg}
+                icon={<Icon w='32px' h='32px' as={FcAdvertising} color={brandColor} />}
+              />
+              <Stat my='auto' ms="10px">
+                <StatNumber color={textColor} fontSize={{ base: "xl" }}>
+                  {data.title}
+                </StatNumber>
+                <StatLabel color={textColor} fontSize="md">
+                  {formatDate(data.description)}
+                </StatLabel>
+              </Stat>
+            </Flex>
+          </Card>
         ))}
       </Box>
     </Box>
