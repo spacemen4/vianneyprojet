c9f884b33a69972a16cd5bc5bee3ca2a5047ff9c
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-03 08:55:56 +0000

    yuyiuyiuyiuy

--- src/views/admin/marketplace/index.jsx
+++ src/views/admin/marketplace/index.jsx
@@ -20,6 +20,7 @@ import CameraForm from "./components/CameraForm";
 import { createClient } from '@supabase/supabase-js';
 import UserForm from './components/UserForm';
 import VianneyAlertChat from '../dataTables/components/VianneyAlertChat';
+import MyCalendar from '../dataTables/components/MyCalendar';
 
 const supabaseUrl = 'https://hvjzemvfstwwhhahecwu.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2anplbXZmc3R3d2hoYWhlY3d1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTQ4Mjc3MCwiZXhwIjoyMDA3MDU4NzcwfQ.6jThCX2eaUjl2qt4WE3ykPbrh6skE8drYcmk-UCNDSw';
@@ -88,6 +89,7 @@ export default function Marketplace() {
           <Box mt="10px" borderRadius="lg" overflow="hidden">
             <MapComponent />
           </Box>
+          <MyCalendar/>
           <Flex direction='column'>
             <Flex
               mt='45px'
