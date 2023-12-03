f1fb9130314a10e9e78c12603c77321911b7e6bb
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-03 11:34:32 +0000

    gffjhhjkghkjj

--- src/views/admin/dataTables/components/TeamSchedule.js
+++ src/views/admin/dataTables/components/TeamSchedule.js
@@ -10,28 +10,45 @@ const events = [
     title: 'Team Meeting',
     start: new Date(2023, 11, 5, 10, 0),
     end: new Date(2023, 11, 5, 11, 0),
+    user: 'John',
   },
   {
     title: 'Work Shift 1',
     start: new Date(2023, 11, 5, 8, 0),
     end: new Date(2023, 11, 5, 16, 0),
+    user: 'John',
   },
   {
     title: 'Work Shift 2',
     start: new Date(2023, 11, 6, 20, 0),
     end: new Date(2023, 11, 7, 4, 0),
+    user: 'Alice',
   },
 ];
 
 const localizer = momentLocalizer(moment);
 
 function TeamSchedule() {
+  // Group events by user
+  const eventsByUser = events.reduce((acc, event) => {
+    if (!acc[event.user]) {
+      acc[event.user] = [];
+    }
+    acc[event.user].push(event);
+    return acc;
+  }, {});
+
+  const userColumns = Object.entries(eventsByUser).map(([user, userEvents]) => ({
+    title: user,
+    events: userEvents,
+  }));
+
   return (
     <ChakraProvider>
       <Box p={4}>
         <Calendar
           localizer={localizer}
-          events={events}
+          events={userColumns}
           startAccessor="start"
           endAccessor="end"
           style={{ height: 500 }}
