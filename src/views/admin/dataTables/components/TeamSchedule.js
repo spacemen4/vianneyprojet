21fb21ecb22d02c6b18b4064fb32e364c4e05fd6
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-03 14:58:53 +0000

    ffgfghgfhgfffg

--- src/views/admin/dataTables/components/TeamSchedule.js
+++ src/views/admin/dataTables/components/TeamSchedule.js
@@ -7,50 +7,51 @@ import moment from 'moment';
 // Example event data
 const events = [
   {
-    title: 'Team Meeting',
+    title: 'Team Meeting - John',
     start: new Date(2023, 11, 5, 10, 0),
     end: new Date(2023, 11, 5, 11, 0),
     user: 'John',
   },
   {
-    title: 'Work Shift 1',
+    title: 'Work Shift 1 - John',
     start: new Date(2023, 11, 5, 8, 0),
     end: new Date(2023, 11, 5, 16, 0),
     user: 'John',
   },
   {
-    title: 'Work Shift 2',
-    start: new Date(2023, 11, 6, 20, 0),
-    end: new Date(2023, 11, 7, 4, 0),
+    title: 'Work Shift 2 - Alice',
+    start: new Date(2023, 11, 5, 8, 0),
+    end: new Date(2023, 11, 5, 16, 0),
     user: 'Alice',
   },
 ];
+const eventStyleGetter = (event) => {
+  const userColors = {
+    'John': 'blue',
+    'Alice': 'green',
+    // Add more users and their associated colors here
+  };
+
+  return {
+    style: {
+      backgroundColor: userColors[event.user] || 'lightgrey', // Default color if user not found
+    },
+  };
+};
 
 const localizer = momentLocalizer(moment);
 
 function TeamSchedule() {
-  // Group events by user
-  const eventsByUser = events.reduce((acc, event) => {
-    if (!acc[event.user]) {
-      acc[event.user] = [];
-    }
-    acc[event.user].push(event);
-    return acc;
-  }, {});
-
-  const userColumns = Object.entries(eventsByUser).map(([user, userEvents]) => ({
-    title: user,
-    events: userEvents,
-  }));
-
+  // No need to group events by user, pass them directly
   return (
     <ChakraProvider>
       <Box p={4}>
         <Calendar
           localizer={localizer}
-          events={userColumns}
+          events={events}
           startAccessor="start"
           endAccessor="end"
+          eventPropGetter={eventStyleGetter}
           style={{ height: 500 }}
         />
       </Box>
