7d2b1483c5e497c3e0c7a46e66682b4acb762dc7
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-03 10:43:22 +0000

    gbbngfgbfgbfgbfg

--- src/views/admin/dataTables/components/CreateTeam.js
+++ src/views/admin/dataTables/components/CreateTeam.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Textarea,
-    VStack,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  VStack,
+  Alert, AlertIcon,
 } from '@chakra-ui/react';
 import { createClient } from '@supabase/supabase-js';
 
@@ -16,77 +17,91 @@ const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYm
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function CreateTeam() {
-    const [teamName, setTeamName] = useState('');
-    const [teamMembers, setTeamMembers] = useState('');
-    const [teamCharacteristics, setTeamCharacteristics] = useState('');
+  const [teamName, setTeamName] = useState('');
+  const [teamMembers, setTeamMembers] = useState('');
+  const [teamCharacteristics, setTeamCharacteristics] = useState('');
+  const [alertStatus, setAlertStatus] = useState(''); // 'success' or 'error'
+  const [alertMessage, setAlertMessage] = useState('');
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-      
-        try {
-          const { data, error } = await supabase
-            .from('vianney_teams')
-            .upsert([
-              {
-                id: uuidv4(), // Generate a UUID for the id
-                name_of_the_team: teamName,
-                persons_working_in_team: teamMembers.split(',').map((member) => member.trim()),
-                characteristics_of_the_team: teamCharacteristics.split(',').map((char) => char.trim()),
-              },
-            ]);
-      
-          if (error) {
-            console.error('Error creating team:', error);
-          } else {
-            console.log('Team created successfully:', data);
-            // Clear form fields or navigate to a different page
-            setTeamName('');
-            setTeamMembers('');
-            setTeamCharacteristics('');
-          }
-        } catch (error) {
-          console.error('Error connecting to Supabase:', error);
-        }
-      };
-      
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    return (
-        <Box>
-            <form onSubmit={handleSubmit}>
-                <VStack spacing={4}>
-                    <FormControl id="teamName">
-                        <FormLabel>Team Name</FormLabel>
-                        <Input
-                            type="text"
-                            value={teamName}
-                            onChange={(e) => setTeamName(e.target.value)}
-                            required
-                        />
-                    </FormControl>
-                    <FormControl id="teamMembers">
-                        <FormLabel>Team Members</FormLabel>
-                        <Input
-                            type="text"
-                            value={teamMembers}
-                            onChange={(e) => setTeamMembers(e.target.value)}
-                            required
-                        />
-                    </FormControl>
-                    <FormControl id="teamCharacteristics">
-                        <FormLabel>Team Characteristics</FormLabel>
-                        <Textarea
-                            value={teamCharacteristics}
-                            onChange={(e) => setTeamCharacteristics(e.target.value)}
-                            rows={4}
-                        />
-                    </FormControl>
-                    <Button type="submit" colorScheme="teal">
-                        Create Team
-                    </Button>
-                </VStack>
-            </form>
-        </Box>
-    );
+    try {
+      const { data, error } = await supabase
+        .from('vianney_teams')
+        .upsert([
+          {
+            id: uuidv4(),
+            name_of_the_team: teamName,
+            persons_working_in_team: teamMembers.split(',').map((member) => member.trim()),
+            characteristics_of_the_team: teamCharacteristics.split(',').map((char) => char.trim()),
+          },
+        ]);
+
+      if (error) {
+        setAlertStatus('error');
+        setAlertMessage('Error creating team. Please try again.');
+      } else {
+        setAlertStatus('success');
+        setAlertMessage('Team created successfully.');
+
+        // Clear form fields or navigate to a different page
+        setTeamName('');
+        setTeamMembers('');
+        setTeamCharacteristics('');
+      }
+    } catch (error) {
+      console.error('Error connecting to Supabase:', error);
+      setAlertStatus('error');
+      setAlertMessage('Error connecting to Supabase. Please try again.');
+    }
+  };
+
+
+
+  return (
+    <Box>
+      <form onSubmit={handleSubmit}>
+        <VStack spacing={4}>
+          <FormControl id="teamName">
+            <FormLabel>Team Name</FormLabel>
+            <Input
+              type="text"
+              value={teamName}
+              onChange={(e) => setTeamName(e.target.value)}
+              required
+            />
+          </FormControl>
+          <FormControl id="teamMembers">
+            <FormLabel>Team Members</FormLabel>
+            <Input
+              type="text"
+              value={teamMembers}
+              onChange={(e) => setTeamMembers(e.target.value)}
+              required
+            />
+          </FormControl>
+          <FormControl id="teamCharacteristics">
+            <FormLabel>Team Characteristics</FormLabel>
+            <Textarea
+              value={teamCharacteristics}
+              onChange={(e) => setTeamCharacteristics(e.target.value)}
+              rows={4}
+            />
+          </FormControl>
+          <Button type="submit" colorScheme="teal">
+            Create Team
+          </Button>
+          {alertStatus && (
+            <Alert status={alertStatus}>
+              <AlertIcon />
+              {alertMessage}
+            </Alert>
+          )}
+        </VStack>
+      </form>
+    </Box>
+  );
 }
 
 export default CreateTeam;
