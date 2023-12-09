896f8fb6ddaa8c5613335e70b956c453a5f3ffed
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-09 21:05:41 +0000

    gfgdfgdfgfg

--- src/views/admin/marketplace/components/EquipiersTable.js
+++ src/views/admin/marketplace/components/EquipiersTable.js
@@ -54,16 +54,25 @@ const EquipiersTable = () => {
           <p>Team: {equipier.name_of_the_team}</p>
           <p>Leader: {leaderName}</p>
           {/* ... other details ... */}
-          <div id={mapId} style={{ height: '150px', width: '100%' }}></div>
+          <div id={mapId} style={{ height: '250px', width: '100%' }}></div> {/* Increased height */}
         </>
       ),
       status: 'info',
       duration: null, // Keeps the toast open until closed manually
       isClosable: true,
       position: "top",
-      onCloseComplete: () => L.map(mapId).remove(), // Clean up the map when toast is closed
-      onRender: () => {
-        // Initialize the map after the toast is rendered
+      onCloseComplete: () => {
+        const mapElement = L.DomUtil.get(mapId);
+        if (mapElement) {
+          mapElement._leaflet_map = undefined; // Ensures proper cleanup
+        }
+      }
+    });
+  
+    // Defer map initialization
+    setTimeout(() => {
+      const mapContainer = document.getElementById(mapId);
+      if (mapContainer && !mapContainer._leaflet_map) { // Check if map is not already initialized
         const map = L.map(mapId).setView([equipier.latitude, equipier.longitude], 13);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           maxZoom: 19,
@@ -71,9 +80,11 @@ const EquipiersTable = () => {
         }).addTo(map);
         L.marker([equipier.latitude, equipier.longitude]).addTo(map);
       }
-    });
+    }, 500); // Adjust timeout as needed
   };
   
+
+
   return (
     <TableContainer>
       <Table variant='simple'>
@@ -83,12 +94,12 @@ const EquipiersTable = () => {
             <Th>Nom de l'équipe</Th>
             <Th>Nom du Leader</Th>
             <Th>Mission</Th> {/* Assuming you have a 'mission' field */}
-</Tr>
+          </Tr>
         </Thead>
         <Tbody>
           {equipiers.map((equipier, index) => (
             <Tr key={index} onClick={() => showDetails(equipier)} cursor="pointer">
-              <Td><img src={equipier.photo_profile_url} alt="Profile" style={{ width: '50px', height: '50px' }}/></Td>
+              <Td><img src={equipier.photo_profile_url} alt="Profile" style={{ width: '50px', height: '50px' }} /></Td>
               <Td>{equipier.name_of_the_team}</Td>
               <Td>{getLeaderName(equipier.team_members)}</Td>
               <Td>{equipier.mission}</Td> {/* Example field */}
