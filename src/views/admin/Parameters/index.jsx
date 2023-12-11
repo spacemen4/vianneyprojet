cafbce7cc070fdc4471c36c62bc04647e3470ca6
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2023-12-11 18:46:37 +0000

    hyjhyjnhgjghjhhgj

--- src/views/admin/Parameters/index.jsx
+++ src/views/admin/Parameters/index.jsx
@@ -1,25 +1,42 @@
 // src/views/admin/Parameters/index.jsx
 import React, { useState } from 'react';
 import { Box, Heading, Button } from '@chakra-ui/react';
+import { IoIosArrowBack } from "react-icons/io";
 import ParametersDocuments from './components/ParametersDocuments';
 import ParametersEventAndCharacteristics from './components/ParametersEventAndCharacteristics';
 import ParametersEvents from './components/ParametersEvents';
+
 const Parameters = () => {
   const [showEventAndCharacteristics, setShowEventAndCharacteristics] = useState(false);
+  const [showDocuments, setShowDocuments] = useState(true);
 
-  const handleButtonClick = () => {
+  const handleEventAndCharacteristicsClick = () => {
     setShowEventAndCharacteristics(true);
+    setShowDocuments(false);
+  };
+
+  const handleBackClick = () => {
+    setShowEventAndCharacteristics(false);
+    setShowDocuments(true);
   };
 
   return (
     <Box p={5}>
       <Heading mb={5}>Admin Parameters</Heading>
-      {
-        showEventAndCharacteristics ?
-        <ParametersEventAndCharacteristics /> :
-        <ParametersDocuments onEventAndCharacteristicsClick={handleButtonClick} />
+      {showDocuments && 
+        <ParametersDocuments onEventAndCharacteristicsClick={handleEventAndCharacteristicsClick} />
       }
-      <ParametersEvents />
+      {showEventAndCharacteristics &&
+        <>
+          <Button leftIcon={<IoIosArrowBack />} onClick={handleBackClick}>
+            Back
+          </Button>
+          <ParametersEventAndCharacteristics />
+        </>
+      }
+      {!showEventAndCharacteristics && !showDocuments &&
+        <ParametersEvents />
+      }
     </Box>
   );
 };
