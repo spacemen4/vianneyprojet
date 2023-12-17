3b1f154c1acaf8f98f603866a5a033eb6bab8ab3
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-17 18:44:35 +0000

    ghhfghgfhfghfg

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -6,30 +6,44 @@ import {
   Text,
   Button,
   Divider,
-  VStack
+  VStack,
+  useColorModeValue
 } from "@chakra-ui/react";
 
-const ServiceSection = ({ title, children }) => (
-  <Box p={5} shadow="md" borderWidth="1px">
-    <Heading fontSize="xl">{title}</Heading>
-    <Text mt={4}>{children}</Text>
-  </Box>
-);
+const ServiceSection = ({ title, children }) => {
+  const bgGradient = useColorModeValue(
+    "linear(to-r, blue.200, cyan.200)", // Light mode gradient
+    "linear(to-r, blue.600, cyan.600)"  // Dark mode gradient
+  );
 
-const CompanyPresentation = () => (
-  <Box pt={{ base: "140px", md: "80px", xl: "80px" }}>
-  <Container maxW="container.xl">
-    <Heading as="h1" size="2xl" textAlign="center" my={5}>
-      Advance Capital
-    </Heading>
-    <Text fontSize="lg" mb={5}>
-      Conseil financier & opérationnel depuis 2007. Accompagnant les entreprises dans leur développement.
-    </Text>
+  return (
+    <Box p={5} shadow="md" borderWidth="1px" bgGradient={bgGradient}>
+      <Heading fontSize="xl">{title}</Heading>
+      <Text mt={4}>{children}</Text>
+    </Box>
+  );
+};
 
-    <Divider my={5} />
+const CompanyPresentation = () => {
+  const containerBgGradient = useColorModeValue(
+    "linear(to-l, blue.300, blue.500)", // Light mode gradient
+    "linear(to-l, blue.700, blue.900)"  // Dark mode gradient
+  );
 
-    <VStack spacing={4} align="stretch">
-      <ServiceSection title="Due Diligences financières">
+  return (
+    <Box pt={{ base: "140px", md: "80px", xl: "80px" }} bgGradient={containerBgGradient}>
+      <Container maxW="container.xl" py={10} borderRadius="lg" bg="whiteAlpha.800" boxShadow="lg">
+        <Heading as="h1" size="2xl" textAlign="center" my={5} color="gray.700">
+          Advance Capital
+        </Heading>
+        <Text fontSize="lg" mb={5} color="gray.600">
+          Conseil financier & opérationnel depuis 2007. Accompagnant les entreprises dans leur développement.
+        </Text>
+
+        <Divider my={5} />
+
+        <VStack spacing={4} align="stretch">
+          <ServiceSection title="Due Diligences financières">
         Conseil à l’acquisition ou à la cession. Une approche détaillée pour assurer une transaction sans heurt.
       </ServiceSection>
 
@@ -46,9 +60,10 @@ const CompanyPresentation = () => (
       <ServiceSection title="Évaluation">
         Evaluations financières de fonds propres ou d’actifs incorporels (PPA), conseil en instruments juridiques de Management Packages.
       </ServiceSection>
-    </VStack>
-  </Container>
-  </Box>
-);
+        </VStack>
+      </Container>
+    </Box>
+  );
+};
 
 export default CompanyPresentation;
