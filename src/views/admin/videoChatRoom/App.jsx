e276e332a8a008a0d6a0101c4cee3486186f3602
lizardspace <154006119+lizardspace@users.noreply.github.com>
2023-12-17 08:33:32 +0000

    thgtfhgfhjgfjgh

--- src/views/admin/videoChatRoom/App.jsx
+++ src/views/admin/videoChatRoom/App.jsx
@@ -4,19 +4,6 @@ import {
   Box,
 } from "@chakra-ui/react";
 
-function randomID(len) {
-  let result = '';
-  if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-    maxPos = chars.length,
-    i;
-  len = len || 5;
-  for (i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return result;
-}
-
 export function getUrlParams(
   url = window.location.href
 ) {
@@ -25,42 +12,43 @@ export function getUrlParams(
 }
 
 export default function App() {
-  const roomID = getUrlParams().get('roomID') || randomID(5);
+  const roomID = "999";
+  const userID = Math.floor(Math.random() * 10000) + "";
+  const userName = "userName" + userID;
+  // generate Kit Token
+  const appID = 696234099;
+  const serverSecret = "ccc867713caf911d2d58684e248c9382";
+  const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userID, userName);
   let myMeeting = async (element) => {
 
- // generate Kit Token
- const appID = 696234099;
- const serverSecret = "ccc867713caf911d2d58684e248c9382";
- const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
-
- // Create instance object from Kit Token.
- const zp = ZegoUIKitPrebuilt.create(kitToken);
- // start the call
- zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url:
-             window.location.protocol + '//' + 
-             window.location.host + window.location.pathname +
-              '?roomID=' +
-              roomID,
-          },
-        ],
-        scenario: {
-         mode: ZegoUIKitPrebuilt.VideoConference,
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url:
+            window.location.protocol + '//' +
+            window.location.host + window.location.pathname +
+            '?roomID=' +
+            roomID,
         },
-   });
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.VideoConference,
+      },
+    });
   };
 
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-    <div
-      className="myCallContainer"
-      ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
-    ></div>
+      <div
+        className="myCallContainer"
+        ref={myMeeting}
+        style={{ width: '100vw', height: '100vh' }}
+      ></div>
     </Box>
   );
 }
