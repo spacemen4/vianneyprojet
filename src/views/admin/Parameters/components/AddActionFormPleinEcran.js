9bb3fe4e72cdaad5740fe22326808171d8ac6a95
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-24 08:50:19 +0000

    feergtregthgfhg

---
+++ src/views/admin/Parameters/components/AddActionFormPleinEcran.js
@@ -0,0 +1,120 @@
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { Box, Button, FormControl, FormLabel, Input, Select, Alert, AlertIcon, AlertTitle, AlertDescription, CloseButton } from '@chakra-ui/react';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const AddActionFormPleinEcran = () => {
+  const [teams, setTeams] = useState([]);
+  const [action, setAction] = useState({
+    teamId: '',
+    actionName: '',
+    startingDateTime: '',
+    endingDateTime: '',
+    comment: ''
+  });
+  const [alert, setAlert] = useState({ status: '', message: '', isVisible: false });
+
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const { data, error } = await supabase.from('vianney_teams').select('*');
+      if (error) {
+        console.error('Erreur lors de la récupération des équipes:', error);
+      } else {
+        setTeams(data);
+      }
+    };
+
+    fetchTeams();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const newAction = {
+      id: uuidv4(),
+      team_to_which_its_attached: action.teamId,
+      action_name: action.actionName,
+      starting_date: action.startingDateTime,
+      ending_date: action.endingDateTime,
+      action_comment: action.comment
+    };
+  
+    const { error } = await supabase
+      .from('vianney_actions')
+      .insert([newAction]);
+  
+    if (error) {
+      console.error('Erreur lors de l insertion des données: ', error);
+      setAlert({
+        status: 'error',
+        message: "Un problème est survenu lors de l'ajout de l'action.",
+        isVisible: true
+      });
+    } else {
+      setAlert({
+        status: 'success',
+        message: "L'action a été ajoutée avec succès.",
+        isVisible: true
+      });
+      setAction({
+        teamId: '',
+        actionName: '',
+        startingDateTime: '',
+        endingDateTime: '',
+        comment: ''
+      });
+    }
+  };
+
+  const closeAlert = () => {
+    setAlert({ ...alert, isVisible: false });
+  };
+
+  return (
+    <Box p={4}>
+      {alert.isVisible && (
+        <Alert status={alert.status} mb={4}>
+          <AlertIcon />
+          <Box flex="1">
+            <AlertTitle>{alert.status === 'error' ? 'Erreur!' : 'Succès!'}</AlertTitle>
+            <AlertDescription display="block">{alert.message}</AlertDescription>
+          </Box>
+          <CloseButton position="absolute" right="8px" top="8px" onClick={closeAlert} />
+        </Alert>
+      )}
+      <form onSubmit={handleSubmit}>
+        <FormControl isRequired>
+          <FormLabel>Équipe</FormLabel>
+          <Select placeholder="Sélectionner une équipe" onChange={(e) => setAction({ ...action, teamId: e.target.value })}>
+            {teams.map(team => (
+              <option key={team.id} value={team.id}>{team.nom}</option>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl isRequired mt={4}>
+          <FormLabel>Nom de l'action</FormLabel>
+          <Input placeholder="Nom de l'action" onChange={(e) => setAction({ ...action, actionName: e.target.value })} />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>Date de début</FormLabel>
+          <Input type="datetime-local" onChange={(e) => setAction({ ...action, startingDateTime: e.target.value })} />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>Date de fin</FormLabel>
+          <Input type="datetime-local" onChange={(e) => setAction({ ...action, endingDateTime: e.target.value })} />
+        </FormControl>
+        <FormControl mt={4}>
+          <FormLabel>Commentaire</FormLabel>
+          <Input placeholder="Commentaire" onChange={(e) => setAction({ ...action, comment: e.target.value })} />
+        </FormControl>
+        <Button mt={4} colorScheme="blue" type="submit">Ajouter l'action</Button>
+      </form>
+    </Box>
+  );
+};
+
+export default AddActionFormPleinEcran;
--- src/views/admin/Parameters/components/ParametersDocuments.js
+++ src/views/admin/Parameters/components/ParametersDocuments.js
@@ -1,9 +1,8 @@
-// src/views/admin/Parameters/components/ParametersDocuments.js
 import React from 'react';
 import { Box, Text, Button, useColorModeValue } from '@chakra-ui/react';
-import { FcAdvertising, FcBusinessman, FcDepartment, FcCalendar } from "react-icons/fc";
+import { FcAdvertising, FcBusinessman } from "react-icons/fc";
 
-const ParametersDocuments = ({ onEventAndCharacteristicsClick }) => {
+const ParametersDocuments = ({ onEventAndCharacteristicsClick, onAddActionClick }) => {
   const brandColor = useColorModeValue("brand.500", "white");
   const buttonBg = useColorModeValue("white", "gray.800");
   const buttonTextColor = useColorModeValue("secondaryGray.900", "white");
@@ -19,40 +18,18 @@ const ParametersDocuments = ({ onEventAndCharacteristicsClick }) => {
         m={4}
         onClick={onEventAndCharacteristicsClick}
       >
-        Ajouter un colaborateur
+        Ajouter un collaborateur
       </Button>
       <Button
         leftIcon={<FcBusinessman size='32px' color={brandColor} />}
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin bottom for spacing
+        m={4} 
+        onClick={onAddActionClick} // Ensure this is calling the correct function
       >
-  
-        Personnels
+        Ajouter une action
       </Button>
-
-      <Button
-        leftIcon={<FcDepartment size='32px' color={brandColor} />}
-        bg={buttonBg}
-        color={buttonTextColor}
-        h='100px'
-        m={4} // Margin bottom for spacing
-      >
-        Salle de crise
-      </Button>
-
-      <Button
-        leftIcon={<FcCalendar size='32px' color={brandColor} />}
-        bg={buttonBg}
-        color={buttonTextColor}
-        h='100px'
-        m={4} // Margin bottom for spacing
-      >
-        Emploi du temps
-      </Button>
-
-      {/* Add more content here */}
     </Box>
   );
 };
