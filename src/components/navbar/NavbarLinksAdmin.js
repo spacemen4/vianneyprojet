3543ff80da53b85b8907b90e63cf3f934a08eb0b
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-24 11:40:47 +0000

    gfgfggg

--- src/components/navbar/NavbarLinksAdmin.js
+++ src/components/navbar/NavbarLinksAdmin.js
@@ -1,39 +1,57 @@
 // Chakra Imports
 import {
 	Flex,
-	useColorModeValue
-} from '@chakra-ui/react';
-import { SidebarResponsive } from 'components/sidebar/Sidebar';
-import PropTypes from 'prop-types';
-import React from 'react';
-
-import routes from 'routes.js';
-export default function HeaderLinks(props) {
+	useColorModeValue,
+	Box,
+  } from '@chakra-ui/react';
+  import PropTypes from 'prop-types';
+  import React from 'react';
+  import routes from 'routes.js';
+  import { SidebarResponsive } from 'components/sidebar/Sidebar';
+  
+  export default function HeaderLinks(props) {
 	const { secondary } = props;
 	let menuBg = useColorModeValue('white', 'navy.800');
-
+  
 	const shadow = useColorModeValue(
-		'14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
-		'14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
+	  '14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
+	  '14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
 	);
+  
+	// Use a constant to determine whether to render the Flex component
+	const isSmallScreen = window.innerWidth <= 740;
+  
 	return (
-		<Flex
-			w={{ sm: 'auto', md: 'auto' }}
-			alignItems="center"
-			flexDirection="row"
-			bg={menuBg}
-			flexWrap={secondary ? { base: 'wrap', md: 'nowrap' } : 'unset'}
+	  <>
+		{isSmallScreen && (
+		  <Flex
+			maxWidth="40px"
+			bg="white"
+			w="100%" // Set the width to 100% to fill the available space
+			h="100%" // Set the height to 100% to fill the available space
+			flexDirection="column" // Center the content vertically
+			boxShadow={shadow}
 			p="10px"
-			borderRadius="30px"
-			boxShadow={shadow}>
+			borderRadius="10px"
+		  >
 			<SidebarResponsive routes={routes} />
-		</Flex>
+		  </Flex>
+		)}
+  
+		{/* Optionally, you can render something else for larger screens */}
+		{!isSmallScreen && (
+		  <Box w={{ sm: 'auto', md: 'auto' }}>
+			{/* Content for larger screens */}
+		  </Box>
+		)}
+	  </>
 	);
-}
-
-HeaderLinks.propTypes = {
+  }
+  
+  HeaderLinks.propTypes = {
 	variant: PropTypes.string,
 	fixed: PropTypes.bool,
 	secondary: PropTypes.bool,
-	onOpen: PropTypes.func
-};
+	onOpen: PropTypes.func,
+  };
+  
