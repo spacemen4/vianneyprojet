7bc85a16c3421d63a038fbeb943eab0770b2684e
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-25 19:36:27 +0000

    drag_handle

--- src/views/admin/googlecalendar/components/EventModal.js
+++ src/views/admin/googlecalendar/components/EventModal.js
@@ -1,4 +1,7 @@
 import React, { useContext, useState } from "react";
+import { MdClose, MdDelete, MdDragHandle, MdSchedule, MdSegment, MdBookmarkBorder, MdCheck } from "react-icons/md";
+import dayjs from 'dayjs';
+import 'dayjs/locale/fr'; // Import French locale
 import GlobalContext from "../context/GlobalContext";
 
 const labelsClasses = [
@@ -11,24 +14,16 @@ const labelsClasses = [
 ];
 
 export default function EventModal() {
-  const {
-    setShowEventModal,
-    daySelected,
-    dispatchCalEvent,
-    selectedEvent,
-  } = useContext(GlobalContext);
+  const { setShowEventModal, daySelected, dispatchCalEvent, selectedEvent } = useContext(GlobalContext);
 
-  const [title, setTitle] = useState(
-    selectedEvent ? selectedEvent.title : ""
-  );
-  const [description, setDescription] = useState(
-    selectedEvent ? selectedEvent.description : ""
-  );
+  const [title, setTitle] = useState(selectedEvent ? selectedEvent.title : "");
+  const [description, setDescription] = useState(selectedEvent ? selectedEvent.description : "");
   const [selectedLabel, setSelectedLabel] = useState(
-    selectedEvent
-      ? labelsClasses.find((lbl) => lbl === selectedEvent.label)
-      : labelsClasses[0]
+    selectedEvent ? labelsClasses.find((lbl) => lbl === selectedEvent.label) : labelsClasses[0]
   );
+
+  // Set dayjs locale to French
+  dayjs.locale('fr');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -44,35 +39,26 @@ export default function EventModal() {
     } else {
       dispatchCalEvent({ type: "push", payload: calendarEvent });
     }
-
     setShowEventModal(false);
   }
+
   return (
     <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center">
       <form className="bg-white rounded-lg shadow-2xl w-1/4">
         <header className="bg-gray-100 px-4 py-2 flex justify-between items-center">
-          <span className="material-icons-outlined text-gray-400">
-            drag_handle
-          </span>
+          <MdDragHandle className="text-gray-400" />
           <div>
             {selectedEvent && (
-              <span
+              <MdDelete
                 onClick={() => {
-                  dispatchCalEvent({
-                    type: "delete",
-                    payload: selectedEvent,
-                  });
+                  dispatchCalEvent({ type: "delete", payload: selectedEvent });
                   setShowEventModal(false);
                 }}
-                className="material-icons-outlined text-gray-400 cursor-pointer"
-              >
-                delete
-              </span>
+                className="text-gray-400 cursor-pointer"
+              />
             )}
             <button onClick={() => setShowEventModal(false)}>
-              <span className="material-icons-outlined text-gray-400">
-                close
-              </span>
+              <MdClose className="text-gray-400" />
             </button>
           </div>
         </header>
@@ -82,31 +68,25 @@ export default function EventModal() {
             <input
               type="text"
               name="title"
-              placeholder="Add title"
+              placeholder="Ajouter un titre"
               value={title}
               required
               className="pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
               onChange={(e) => setTitle(e.target.value)}
             />
-            <span className="material-icons-outlined text-gray-400">
-              schedule
-            </span>
-            <p>{daySelected.format("dddd, MMMM DD")}</p>
-            <span className="material-icons-outlined text-gray-400">
-              segment
-            </span>
+            <MdSchedule className="text-gray-400" />
+            <p>{daySelected.format("dddd, DD MMMM YYYY")}</p>
+            <MdSegment className="text-gray-400" />
             <input
               type="text"
               name="description"
-              placeholder="Add a description"
+              placeholder="Ajouter une description"
               value={description}
               required
               className="pt-3 border-0 text-gray-600 pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-blue-500"
               onChange={(e) => setDescription(e.target.value)}
             />
-            <span className="material-icons-outlined text-gray-400">
-              bookmark_border
-            </span>
+            <MdBookmarkBorder className="text-gray-400" />
             <div className="flex gap-x-2">
               {labelsClasses.map((lblClass, i) => (
                 <span
@@ -115,9 +95,7 @@ export default function EventModal() {
                   className={`bg-${lblClass}-500 w-6 h-6 rounded-full flex items-center justify-center cursor-pointer`}
                 >
                   {selectedLabel === lblClass && (
-                    <span className="material-icons-outlined text-white text-sm">
-                      check
-                    </span>
+                    <MdCheck className="text-white text-sm" />
                   )}
                 </span>
               ))}
@@ -130,7 +108,7 @@ export default function EventModal() {
             onClick={handleSubmit}
             className="bg-blue-500 hover:bg-blue-600 px-6 py-2 rounded text-white"
           >
-            Save
+            Sauvegarder
           </button>
         </footer>
       </form>
