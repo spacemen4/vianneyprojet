70e3724b3c1c24adc3f77ffe6b412b2360c78817
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-26 15:04:33 +0000

    bvbgbnfgnhnhnhnhnh

--- src/views/admin/googlecalendar/components/EventModal.js
+++ src/views/admin/googlecalendar/components/EventModal.js
@@ -2,7 +2,6 @@ import React, { useContext, useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import 'dayjs/locale/fr';
 import GlobalContext from '../context/GlobalContext';
-import { createClient } from '@supabase/supabase-js';
 import {
   Modal,
   ModalOverlay,
@@ -20,43 +19,55 @@ import {
   Icon,
 } from '@chakra-ui/react';
 import { MdDelete, MdCheck } from 'react-icons/md';
+import { createClient } from '@supabase/supabase-js';
 
-// Initialize Supabase client (replace with your actual configuration)
-const supabaseUrl = 'your-supabase-url';
-const supabaseAnonKey = 'your-anonymous-key';
+const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-const EventModal = () => {
-  const { setShowEventModal, daySelected, selectedEvent } = useContext(GlobalContext);
+export default function EventModal() {
+  const {
+    setShowEventModal,
+    daySelected,
+    dispatchCalEvent,
+    selectedEvent,
+  } = useContext(GlobalContext);
 
-  // State for form fields
-  const [formFields, setFormFields] = useState({
-    title: '',
-    description: '',
-    label: 'indigo',
-    // Additional fields
-    team: '',
-    actionName: '',
-    startDate: '',
-    endDate: '',
-    comment: ''
-  });
+  const [title, setTitle] = useState(selectedEvent ? selectedEvent.title : '');
+  const [description, setDescription] = useState(
+    selectedEvent ? selectedEvent.description : ''
+  );
+  const [selectedLabel, setSelectedLabel] = useState(
+    selectedEvent ? selectedEvent.label : 'indigo'
+  );
+  const labelsColors = {
+    indigo: 'indigo.500',
+    gray: 'gray.500',
+    green: 'green.500',
+    blue: 'blue.500',
+    red: 'red.500',
+    purple: 'purple.500',
+  };
 
-  useEffect(() => {
-    // Pre-fill form if editing an existing event
-    if (selectedEvent) {
-      setFormFields({ ...formFields, ...selectedEvent });
-    }
-  }, [selectedEvent]);
+  const [team, setTeam] = useState(''); // New state for Équipe
+  const [actionName, setActionName] = useState(''); // New state for Nom de l'action
+  const [startDate, setStartDate] = useState(''); // New state for Date de début
+  const [endDate, setEndDate] = useState(''); // New state for Date de fin
+  const [comment, setComment] = useState(''); // New state for Commentaire
 
-  const handleSubmit = async (e) => {
+  dayjs.locale('fr');
+
+  async function handleSubmit(e) {
     e.preventDefault();
     const event = {
-      ...formFields,
+      title,
+      description,
+      label: selectedLabel,
       day: daySelected.valueOf(),
       id: selectedEvent ? selectedEvent.id : Date.now(),
     };
 
+    // Push data to Supabase
     const { error } = selectedEvent
       ? await supabase.from('your-table-name').update(event).match({ id: selectedEvent.id })
       : await supabase.from('your-table-name').insert([event]);
@@ -64,30 +75,47 @@ const EventModal = () => {
     if (error) {
       console.error('Error:', error);
     } else {
+      // Fetch data from Supabase (assuming your table is 'vianney_actions')
+      const { data: actionsData, error: fetchError } = await supabase.from('vianney_actions').select('*');
+      if (fetchError) {
+        console.error('Error fetching data from Supabase:', fetchError);
+      } else {
+        // Dispatch fetched data to update the context state
+        dispatchCalEvent({ type: 'update', payload: actionsData });
+      }
+
       setShowEventModal(false);
     }
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormFields({ ...formFields, [name]: value });
-  };
-
-  // ... Rest of your component code, including modal structure
+  }
 
   return (
     <Modal isOpen={true} onClose={() => setShowEventModal(false)}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{selectedEvent ? 'Edit Event' : 'Create Event'}</ModalHeader>
+        <ModalHeader>{selectedEvent ? 'Edit Event' : 'Create Event'}
+          {selectedEvent && (
+            <Icon
+              as={MdDelete}
+              ml={2}
+              cursor="pointer"
+              onClick={() => {
+                dispatchCalEvent({ type: 'delete', payload: selectedEvent });
+                setShowEventModal(false);
+              }}
+            />
+          )}
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          {/* Form controls for each field */}
-          <FormControl isRequired>
-            <FormLabel>Title</FormLabel>
-            <Input name="title" value={formFields.title} onChange={handleChange} />
+          <FormControl id="event-team" isRequired mt={4}>
+            <FormLabel>Équipe*</FormLabel>
+            <Input
+              placeholder="Enter Team"
+              value={team}
+              onChange={(e) => setTeam(e.target.value)}
+            />
           </FormControl>
-          
+
           <FormControl id="event-action-name" isRequired mt={4}>
             <FormLabel>Nom de l'action*</FormLabel>
             <Input
@@ -145,7 +173,9 @@ const EventModal = () => {
           </FormControl>
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="blue" onClick={handleSubmit}>Save</Button>
+          <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
+            Save
+          </Button>
           <Button variant="ghost" onClick={() => setShowEventModal(false)}>
             Cancel
           </Button>
@@ -153,6 +183,4 @@ const EventModal = () => {
       </ModalContent>
     </Modal>
   );
-};
-
-export default EventModal;
+}
