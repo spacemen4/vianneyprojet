0a7155a52c2b0e44609505a404b83d29da0156ba
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-26 14:56:48 +0000

    gfgdbgdbhfgbfhh

--- src/views/admin/googlecalendar/components/EventModal.js
+++ src/views/admin/googlecalendar/components/EventModal.js
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import dayjs from 'dayjs';
 import 'dayjs/locale/fr';
 import GlobalContext from '../context/GlobalContext';
+import { createClient } from '@supabase/supabase-js';
 import {
   Modal,
   ModalOverlay,
@@ -20,83 +21,73 @@ import {
 } from '@chakra-ui/react';
 import { MdDelete, MdCheck } from 'react-icons/md';
 
-const labelsColors = {
-  indigo: 'indigo.500',
-  gray: 'gray.500',
-  green: 'green.500',
-  blue: 'blue.500',
-  red: 'red.500',
-  purple: 'purple.500',
-};
+// Initialize Supabase client (replace with your actual configuration)
+const supabaseUrl = 'your-supabase-url';
+const supabaseAnonKey = 'your-anonymous-key';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
-export default function EventModal() {
-  const {
-    setShowEventModal,
-    daySelected,
-    dispatchCalEvent,
-    selectedEvent,
-  } = useContext(GlobalContext);
+const EventModal = () => {
+  const { setShowEventModal, daySelected, selectedEvent } = useContext(GlobalContext);
 
-  const [title, setTitle] = useState(selectedEvent ? selectedEvent.title : '');
-  const [description, setDescription] = useState(
-    selectedEvent ? selectedEvent.description : ''
-  );
-  const [selectedLabel, setSelectedLabel] = useState(
-    selectedEvent ? selectedEvent.label : 'indigo'
-  );
+  // State for form fields
+  const [formFields, setFormFields] = useState({
+    title: '',
+    description: '',
+    label: 'indigo',
+    // Additional fields
+    team: '',
+    actionName: '',
+    startDate: '',
+    endDate: '',
+    comment: ''
+  });
 
-  dayjs.locale('fr');
+  useEffect(() => {
+    // Pre-fill form if editing an existing event
+    if (selectedEvent) {
+      setFormFields({ ...formFields, ...selectedEvent });
+    }
+  }, [selectedEvent]);
 
-  function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const calendarEvent = {
-      title,
-      description,
-      label: selectedLabel,
+    const event = {
+      ...formFields,
       day: daySelected.valueOf(),
       id: selectedEvent ? selectedEvent.id : Date.now(),
     };
-    if (selectedEvent) {
-      dispatchCalEvent({ type: 'update', payload: calendarEvent });
+
+    const { error } = selectedEvent
+      ? await supabase.from('your-table-name').update(event).match({ id: selectedEvent.id })
+      : await supabase.from('your-table-name').insert([event]);
+
+    if (error) {
+      console.error('Error:', error);
     } else {
-      dispatchCalEvent({ type: 'push', payload: calendarEvent });
+      setShowEventModal(false);
     }
-    setShowEventModal(false);
-  }
-  const [team, setTeam] = useState(''); // New state for Équipe
-  const [actionName, setActionName] = useState(''); // New state for Nom de l'action
-  const [startDate, setStartDate] = useState(''); // New state for Date de début
-  const [endDate, setEndDate] = useState(''); // New state for Date de fin
-  const [comment, setComment] = useState(''); // New state for Commentaire
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  // ... Rest of your component code, including modal structure
 
   return (
     <Modal isOpen={true} onClose={() => setShowEventModal(false)}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>{selectedEvent ? 'Edit Event' : 'Create Event'}
-        {selectedEvent && (
-            <Icon
-              as={MdDelete}
-              ml={2}
-              cursor="pointer"
-              onClick={() => {
-                dispatchCalEvent({ type: 'delete', payload: selectedEvent });
-                setShowEventModal(false);
-              }}
-            />
-          )}
-        </ModalHeader>
+        <ModalHeader>{selectedEvent ? 'Edit Event' : 'Create Event'}</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>          
-          <FormControl id="event-team" isRequired mt={4}>
-            <FormLabel>Équipe*</FormLabel>
-            <Input
-              placeholder="Enter Team"
-              value={team}
-              onChange={(e) => setTeam(e.target.value)}
-            />
+        <ModalBody>
+          {/* Form controls for each field */}
+          <FormControl isRequired>
+            <FormLabel>Title</FormLabel>
+            <Input name="title" value={formFields.title} onChange={handleChange} />
           </FormControl>
-
+          
           <FormControl id="event-action-name" isRequired mt={4}>
             <FormLabel>Nom de l'action*</FormLabel>
             <Input
@@ -154,9 +145,7 @@ export default function EventModal() {
           </FormControl>
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
-            Save
-          </Button>
+          <Button colorScheme="blue" onClick={handleSubmit}>Save</Button>
           <Button variant="ghost" onClick={() => setShowEventModal(false)}>
             Cancel
           </Button>
@@ -164,4 +153,6 @@ export default function EventModal() {
       </ModalContent>
     </Modal>
   );
-}
+};
+
+export default EventModal;
