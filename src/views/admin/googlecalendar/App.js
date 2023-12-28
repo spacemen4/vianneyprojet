a4e70fe0baa55ac11599a68e2fc76f95b7186b93
lizardspace2 <154059776+lizardspace2@users.noreply.github.com>
2023-12-28 22:38:28 +0000

    hbcfghgchngjnhgjhgfjgfj

--- src/views/admin/googlecalendar/App.js
+++ src/views/admin/googlecalendar/App.js
@@ -1,24 +1,47 @@
 import React, { useState, useContext, useEffect } from "react";
 import { getMonth } from "./util";
-import { ChakraProvider, Flex, Box } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Box, Text, VStack, Tooltip, Grid } from "@chakra-ui/react";
 import CalendarHeader from "./components/CalendarHeader";
 import Sidebar from "./components/Sidebar";
-import Month from "./components/Month";
 import GlobalContext from "./context/GlobalContext";
 import EventModal from "./components/EventModal";
 import dayjs from 'dayjs';
+import isBetween from "dayjs/plugin/isBetween";
 import 'dayjs/locale/fr';
 import CreateEventButton from "./components/CreateEventButton";
 import ModifyActionButton from "./components/ModifyActionButton";
 import ModifyActionButtonBis from "./components/ModifyActionButtonBis";
 import ActionIdDisplay from "./components/ActionIdDisplay"; // Import the ActionIdDisplay component
+import { createClient } from '@supabase/supabase-js';
 
-function App() {
-  const [currenMonth, setCurrentMonth] = useState(getMonth());
+dayjs.locale('fr');
+dayjs.extend(isBetween);
+const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const formatDate = (date) => {
+  return dayjs(date).format('DD/MM/YYYY');
+}
+
+const formatTooltipContent = (event) => {
+  return (
+    <VStack align="left" spacing={1}>
+      <Text>Intervenant: {event.nom || 'N/A'} {event.prenom || 'N/A'}</Text>
+      <Text>Disponible pour: {event.action_name}</Text>
+      <Text>Du: {formatDate(event.starting_date)} au {formatDate(event.ending_date)}</Text>
+      <Text>Commentaire: {event.action_comment || 'N/A'}</Text>
+      <Text>Dernière mise à jour: {formatDate(event.last_updated)}</Text>
+    </VStack>
+  );
+};
+
+const App = () => {
+  const [currentMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext); // Add selectedEvent state
-  const [showModifyForm, setShowModifyForm] = useState(false); 
+  const [showModifyForm, setShowModifyForm] = useState(false);
   const [selectedActionData, setSelectedActionData] = useState(null);
-  dayjs.locale('fr');
+  const { setDaySelected, setShowEventModal, setSelectedEvent, setSelectedAction } = useContext(GlobalContext);
   const modifyActionButtonStyle = {
     display: 'none', // This style will hide the button
   };
@@ -26,12 +49,100 @@ function App() {
     setCurrentMonth(getMonth(monthIndex));
   }, [monthIndex]);
 
+  const DayComponent = ({ day, rowIdx }) => {
+    const [dayEvents, setDayEvents] = useState([]);
+
+    useEffect(() => {
+      const fetchActions = async () => {
+        const { data, error } = await supabase
+          .from('team_action_view_rendering')
+          .select('*');
+
+        if (error) {
+          console.error('Error fetching actions:', error);
+        } else {
+          const events = data.filter(action =>
+            dayjs(day).isBetween(dayjs(action.starting_date).subtract(1, 'day'), dayjs(action.ending_date), null, '[]')
+          );
+          setDayEvents(events);
+        }
+      };
+
+      fetchActions();
+    }, [day]);
+
+    function getCurrentDayClass() {
+      return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+        ? { bg: "blue.600", color: "white", borderRadius: "full", w: "7" }
+        : {};
+    }
+
+    return (
+      <Box border="1px" borderColor="gray.200" flexDir="column" display="flex">
+        <Flex flexDir="column" alignItems="center">
+          {rowIdx === 0 && (
+            <Text fontSize="sm" mt={1}>
+              {day.format("ddd").toUpperCase()}
+            </Text>
+          )}
+          <Text
+            fontSize="sm"
+            p={1}
+            my={1}
+            textAlign="center"
+            {...getCurrentDayClass()}
+          >
+            {day.format("DD")}
+          </Text>
+        </Flex>
+        <Flex
+          flexDir="column"
+          flex={1}
+          cursor="pointer"
+          onClick={() => {
+            setDaySelected(day);
+            setShowEventModal(true);
+          }}
+        >
+          {dayEvents.map((event, idx) => (
+            <Tooltip
+              key={idx}
+              label={formatTooltipContent(event)}
+              fontSize="sm"
+              placement="right"
+              hasArrow
+            >
+              <Box
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setDaySelected(day);
+                  setSelectedEvent(event.id);
+                  setSelectedAction(event); // Pass action data to ModifyActionButton
+                }}
+                bg={event.color || 'gray.200'}
+                p={1}
+                color="gray.600"
+                fontSize="sm"
+                borderRadius="md"
+                mb={1}
+                width="100%"
+                isTruncated
+              >
+                {event.action_name}
+              </Box>
+            </Tooltip>
+          ))}
+        </Flex>
+      </Box>
+    );
+  };
+
   return (
     <ChakraProvider>
       {showEventModal && !showModifyForm && <EventModal />}
       <Flex direction="column" height="100vh">
         <CalendarHeader />
-        
+
         <Flex
           className="main-container"
           flex={1}
@@ -46,18 +157,26 @@ function App() {
               <div style={modifyActionButtonStyle}>
                 <ModifyActionButton initialActionData={selectedActionData} />
               </div>
-              <ModifyActionButtonBis/>
+              <ModifyActionButtonBis />
               <ActionIdDisplay actionId={selectedActionData?.action_id} /> {/* Pass the action_id property */}
             </Flex>
             <Box display={["none", "block"]}>
               <Sidebar />
             </Box>
           </Box>
-          <Month
-            month={currenMonth}
-            setShowModifyForm={setShowModifyForm}
-            setSelectedAction={setSelectedActionData} // Pass the callback down
-          />
+          <Grid flex="1" templateColumns="repeat(7, 1fr)" templateRows="repeat(5, 1fr)" p={1}>
+            {currentMonth.map((row, i) => (
+              <React.Fragment key={i}>
+                {row.map((day, idx) => (
+                  <DayComponent
+                    day={day}
+                    rowIdx={i}
+                    key={idx}
+                  />
+                ))}
+              </React.Fragment>
+            ))}
+          </Grid>
         </Flex>
       </Flex>
     </ChakraProvider>
--- src/views/admin/googlecalendar/components/Month.js
+++ src/views/admin/googlecalendar/components/Month.js
@@ -1,19 +1,132 @@
-import React from "react";
-import Day from "./Day";
-import { Grid } from "@chakra-ui/react";
+import React, { useState, useEffect, useContext } from "react";
+import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
+import GlobalContext from "../context/GlobalContext";
+import { createClient } from '@supabase/supabase-js';
+import { Grid, Box, Text, Flex, Tooltip, VStack } from "@chakra-ui/react";
+
+dayjs.extend(isBetween);
+
+const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function Month({ month, setShowModifyForm, setSelectedAction }) {
+  const { setDaySelected, setShowEventModal, setSelectedEvent } = useContext(GlobalContext);
+
+  const formatDate = (date) => {
+    return dayjs(date).format('DD/MM/YYYY');
+  }
+
+  const formatTooltipContent = (event) => {
+    return (
+      <VStack align="left" spacing={1}>
+        <Text>Intervenant: {event.nom || 'N/A'} {event.prenom || 'N/A'}</Text>
+        <Text>Disponible pour: {event.action_name}</Text>
+        <Text>Du: {formatDate(event.starting_date)} au {formatDate(event.ending_date)}</Text>
+        <Text>Commentaire: {event.action_comment || 'N/A'}</Text>
+        <Text>Dernière mise à jour: {formatDate(event.last_updated)}</Text>
+      </VStack>
+    );
+  };
+
+  const DayComponent = ({ day, rowIdx }) => {
+    const [dayEvents, setDayEvents] = useState([]);
+
+    useEffect(() => {
+      const fetchActions = async () => {
+        const { data, error } = await supabase
+          .from('team_action_view_rendering')
+          .select('*');
+
+        if (error) {
+          console.error('Error fetching actions:', error);
+        } else {
+          const events = data.filter(action =>
+            dayjs(day).isBetween(dayjs(action.starting_date).subtract(1, 'day'), dayjs(action.ending_date), null, '[]')
+          );
+          setDayEvents(events);
+        }
+      };
+
+      fetchActions();
+    }, [day]);
+
+    function getCurrentDayClass() {
+      return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+        ? { bg: "blue.600", color: "white", borderRadius: "full", w: "7" }
+        : {};
+    }
+
+    return (
+      <Box border="1px" borderColor="gray.200" flexDir="column" display="flex">
+        <Flex flexDir="column" alignItems="center">
+          {rowIdx === 0 && (
+            <Text fontSize="sm" mt={1}>
+              {day.format("ddd").toUpperCase()}
+            </Text>
+          )}
+          <Text
+            fontSize="sm"
+            p={1}
+            my={1}
+            textAlign="center"
+            {...getCurrentDayClass()}
+          >
+            {day.format("DD")}
+          </Text>
+        </Flex>
+        <Flex
+          flexDir="column"
+          flex={1}
+          cursor="pointer"
+          onClick={() => {
+            setDaySelected(day);
+            setShowEventModal(true);
+          }}
+        >
+          {dayEvents.map((event, idx) => (
+            <Tooltip
+              key={idx}
+              label={formatTooltipContent(event)}
+              fontSize="sm"
+              placement="right"
+              hasArrow
+            >
+              <Box
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setDaySelected(day);
+                  setSelectedEvent(event.id);
+                  setSelectedAction(event); // Pass action data to ModifyActionButton
+                }}
+                bg={event.color || 'gray.200'}
+                p={1}
+                color="gray.600"
+                fontSize="sm"
+                borderRadius="md"
+                mb={1}
+                width="100%"
+                isTruncated
+              >
+                {event.action_name}
+              </Box>
+            </Tooltip>
+          ))}
+        </Flex>
+      </Box>
+    );
+  };
+
   return (
     <Grid flex="1" templateColumns="repeat(7, 1fr)" templateRows="repeat(5, 1fr)" p={1}>
       {month.map((row, i) => (
         <React.Fragment key={i}>
           {row.map((day, idx) => (
-            <Day
+            <DayComponent
               day={day}
-              key={idx}
               rowIdx={i}
-              setShowModifyForm={setShowModifyForm}
-              setSelectedAction={setSelectedAction} // Pass the callback down
+              key={idx}
             />
           ))}
         </React.Fragment>
