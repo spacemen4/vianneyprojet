f161f42d59501d41f0bdc1c9dc6fb58d68cf6418
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2023-12-31 12:21:39 +0000

    gfrgfdgfdfgf

--- src/views/admin/googlecalendar/components/Sidebar.js
+++ src/views/admin/googlecalendar/components/Sidebar.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SmallCalendar from "./SmallCalendar";
-import Labels from "./Labels";
-import { Box } from "@chakra-ui/react";
-export default function Sidebar() {
+import { Box, Text, Checkbox, Flex, Badge } from "@chakra-ui/react";
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const Sidebar = () => {
+  const [teamMembers, setTeamMembers] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from Supabase when the component mounts
+    async function fetchTeamMembers() {
+      const { data, error } = await supabase.from("vianney_teams").select("*");
+
+      if (error) {
+        console.error("Error fetching team members:", error.message);
+        return;
+      }
+
+      // Store the team members in state
+      setTeamMembers(data);
+    }
+
+    fetchTeamMembers();
+  }, []);
+
+  const handleCheckboxChange = (index) => {
+    // Handle checkbox state changes if needed
+    // You can manage the checkbox state here
+    // For simplicity, this example doesn't include checkbox state management
+  };
+
   return (
     <Box>
-
       <SmallCalendar />
-      <Labels />
+      <Text fontSize="xl" fontWeight="bold">Team Members:</Text>
+      <ul>
+        {teamMembers.map((member, index) => (
+          <li key={index}>
+            <Flex alignItems="center">
+              <Checkbox
+                onChange={() => handleCheckboxChange(index)}
+                isChecked={true} 
+              />
+              <Badge marginLeft="2" color={member.color || "blue"}>{`${member.nom} ${member.prenom}`}</Badge>
+            </Flex>
+          </li>
+        ))}
+      </ul>
     </Box>
   );
-}
+};
+
+export default Sidebar;
