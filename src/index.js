8776eb41f33749d7459f5d17898c4ee524f80ecf
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-01-07 18:29:03 +0000

    hjhgjhgjhgjhgj

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/css/App.css';
-import { HashRouter, Route, Switch, Redirect, Link } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import AuthLayout from 'layouts/auth';
 import AdminLayout from 'layouts/admin';
 import RtlLayout from 'layouts/rtl';
