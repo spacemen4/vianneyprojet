727a01646b6d3ecf5004451d8147ed8c68583776
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-01-09 20:53:35 +0000

    thgtgtrtrtrtrtr

--- src/views/admin/YourClientBookingComponent/components/AddActionFormPleinEcran.js
+++ src/views/admin/YourClientBookingComponent/components/AddActionFormPleinEcran.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { ChakraProvider, Alert, AlertIcon, Text, Badge, Flex, VStack, Icon } from "@chakra-ui/react";
-import { FaCalendar, FaComment } from 'react-icons/fa'; // Import the Font Awesome icons
+import { ChakraProvider, Alert, AlertIcon, Text, Badge, Flex, VStack, Icon, Grid } from "@chakra-ui/react";
+import { FaCalendar, FaComment } from 'react-icons/fa';
 import { createClient } from '@supabase/supabase-js';
+import { FcCalendar, FcVoicePresentation } from "react-icons/fc";
 
-import { FcCurrencyExchange, FcCalendar, FcVoicePresentation } from "react-icons/fc"; // Import the Font Awesome icons
 const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 function DisplayVianneyActions() {
-  const [actions, setActions] = useState([]);
+  const [reservedActions, setReservedActions] = useState([]);
+  const [nonReservedActions, setNonReservedActions] = useState([]);
 
   useEffect(() => {
     async function fetchActions() {
@@ -20,19 +21,19 @@ function DisplayVianneyActions() {
         return;
       }
 
+      // Filter actions based on reserved_action
+      const reservedActions = data.filter(action => action.reserved_action === true);
+      const nonReservedActions = data.filter(action => action.reserved_action !== true);
+
       // Format the date to French locale
-      const formattedActions = data.map((action) => {
-        const startingDate = new Date(action.starting_date).toLocaleDateString('fr-FR');
-        const endingDate = new Date(action.ending_date).toLocaleDateString('fr-FR');
+      const formatActions = actions => actions.map(action => ({
+        ...action,
+        starting_date: new Date(action.starting_date).toLocaleDateString('fr-FR'),
+        ending_date: new Date(action.ending_date).toLocaleDateString('fr-FR'),
+      }));
 
-        return {
-          ...action,
-          starting_date: startingDate,
-          ending_date: endingDate,
-        };
-      });
-
-      setActions(formattedActions);
+      setReservedActions(formatActions(reservedActions));
+      setNonReservedActions(formatActions(nonReservedActions));
     }
 
     fetchActions();
@@ -40,19 +41,32 @@ function DisplayVianneyActions() {
 
   return (
     <ChakraProvider>
-      <div>
-        {actions.map((action) => (
-          <Alert
-            key={action.id}
-            status="info"
-            borderRadius="md"
-            mb={2}
-            bgColor="gray.100" // Background color
-            color="gray.900" // Text color
-          >
-            <AlertIcon />
-            <VStack align="start">
-              <Flex alignItems="center">
+      <Grid templateColumns="repeat(2, 1fr)" gap={4}>
+        <ColumnActions actions={reservedActions} title="Reserved Actions" />
+        <ColumnActions actions={nonReservedActions} title="Non-Reserved Actions" />
+      </Grid>
+    </ChakraProvider>
+  );
+}
+
+function ColumnActions({ actions, title }) {
+  return (
+    <div>
+      <Text fontSize="xl" fontWeight="bold" mb={4}>
+        {title}
+      </Text>
+      {actions.map((action) => (
+        <Alert
+          key={action.id}
+          status="info"
+          borderRadius="md"
+          mb={2}
+          bgColor="gray.100" // Background color
+          color="gray.900" // Text color
+        >
+          <AlertIcon />
+          <VStack align="start">
+            <Flex alignItems="center">
                 <Badge colorScheme="teal" fontSize="0.8em" mr={2}>
                   {action.action_name}
                 </Badge>
@@ -78,11 +92,10 @@ function DisplayVianneyActions() {
                 </Text>
                 <Text fontWeight="bold">{action.action_comment}</Text>
               </Flex>
-            </VStack>
-          </Alert>
-        ))}
-      </div>
-    </ChakraProvider>
+          </VStack>
+        </Alert>
+      ))}
+    </div>
   );
 }
 
