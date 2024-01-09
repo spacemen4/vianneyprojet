31988dea4ee7923d163f0314b1b7f8d38d20c254
spacemen4 <155259834+spacemen4@users.noreply.github.com>
2024-01-09 20:26:04 +0000

    fgfdhhgfhgfgfhg

--- src/views/admin/YourClientBookingComponent/components/AddActionFormPleinEcran.js
+++ src/views/admin/YourClientBookingComponent/components/AddActionFormPleinEcran.js
@@ -3,6 +3,7 @@ import { ChakraProvider, Alert, AlertIcon, Text, Badge, Flex, VStack, Icon } fro
 import { FaCalendar, FaComment } from 'react-icons/fa'; // Import the Font Awesome icons
 import { createClient } from '@supabase/supabase-js';
 
+import { FcCurrencyExchange, FcCalendar, FcVoicePresentation } from "react-icons/fc"; // Import the Font Awesome icons
 const supabaseUrl = 'https://pvpsmyizvorwwccuwbuq.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2cHNteWl6dm9yd3djY3V3YnVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMjgzMDg2MCwiZXhwIjoyMDE4NDA2ODYwfQ.9YDEN41__xBFJU91XY9e3r119A03yQ2oq5azmrx1aqY';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
@@ -53,30 +54,30 @@ function DisplayVianneyActions() {
             <VStack align="start">
               <Flex alignItems="center">
                 <Badge colorScheme="teal" fontSize="0.8em" mr={2}>
-                {action.action_name}
+                  {action.action_name}
                 </Badge>
               </Flex>
-            <Flex alignItems="center">
-              <Icon as={FaCalendar} boxSize={6} mr={2} />
-              <Text margin="2">
-                Starting Date:
-              </Text>
-              <Text fontWeight="bold">{action.starting_date}</Text>
-            </Flex>
-            <Flex alignItems="center">
-              <Icon as={FaCalendar} boxSize={6} mr={2} />
-              <Text margin="2">
-                Ending Date:
-              </Text>
-              <Text fontWeight="bold">{action.ending_date}</Text>
-            </Flex>
-            <Flex alignItems="center">
-                <Icon as={FaComment} boxSize={6} mr={2} />
+              <Flex alignItems="center">
+                <Icon as={FcCalendar} boxSize={6} mr={2} />
+                <Text margin="2">
+                  Starting Date:
+                </Text>
+                <Text fontWeight="bold">{action.starting_date}</Text>
+              </Flex>
+              <Flex alignItems="center">
+                <Icon as={FcCalendar} boxSize={6} mr={2} />
+                <Text margin="2">
+                  Ending Date:
+                </Text>
+                <Text fontWeight="bold">{action.ending_date}</Text>
+              </Flex>
+              <Flex alignItems="center">
+                <Icon as={FcVoicePresentation} boxSize={6} mr={2} />
                 <Text margin="2">
                   Action Comment:
                 </Text>
                 <Text fontWeight="bold">{action.action_comment}</Text>
-              </Flex>              
+              </Flex>
             </VStack>
           </Alert>
         ))}
